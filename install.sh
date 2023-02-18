1fabf4cdb782ffba3737de8437a0434e7604aad4
qt <124061508+qtTF2@users.noreply.github.com>
2023-02-18 14:37:42 +0100

    fix steam paths for debian/ubuntu

--- install.sh
+++ install.sh
@@ -1,7 +1,42 @@
 #!/usr/bin/env bash
 loc=$(dirname $0)
 INTERFACE=$(ip route get 8.8.8.8 | awk -- '{printf $5}') 
-#checking dependencies first
+#os checking
+OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+if [ -z "$OS" ]; then
+    OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
+fi
+
+# FUCK YOU DEBIAN/UBUNTU
+# YOU FUCK FIREJAIL NETWORK CONFIGURATION BY DISABLING NETWORKING BY DEFAULT, FUCK YOU.
+STEAM_PATH=""
+
+echo "(!) Checking your linux distro for available fixes..."
+if [ $OS == "ubuntu" ]; then
+     echo "(?) Your steam path is different. This is why you weren't able to see TF2."
+     echo "(!) Fixing wrong steam paths..."
+     STEAM_PATH="/home/$USER/.steam/steam"
+     #not sure if this automatically fixes the firejail problem
+     sudo echo "restricted-network no" > sudo /etc/firejail/default.profile
+fi
+
+#not tested
+#debian and ubuntu might be the same i dont see any difference between ubuntu and debian.
+if [ $OS == "debian" ]; then
+     echo "(!) Fixing wrong steam paths..."
+     echo "(?) Your steam path is different. This is why you weren't able to see TF2."
+     STEAM_PATH="/home/$USER/.steam/steam"
+     #not sure if this automatically fixes the firejail problem
+     sudo echo "restricted-network no" > sudo /etc/firejail/default.profile
+fi
+
+#not tested
+if [ $OS == "arch" ]; then
+     echo "(?) Your steam path did not change. You should see TF2 now."
+     STEAM_PATH="/home/$USER/.local/share/Steam/"
+fi
+
+mkdir db
 if [ ! -x "$(command -v pactl)" ]; then
     echo "(!) pactl doesn't exist, exiting..."
     exit
@@ -17,44 +52,6 @@ if [ ! -x "$(command -v firejail)" ]; then
     echo "(!) Firejail doesn't exist, exiting..."
     exit
 fi
-
-#os checking
-OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
-if [ -z "$OS" ]; then
-    OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
-fi
-
-# FUCK YOU DEBIAN/UBUNTU
-# YOU FUCK FIREJAIL NETWORK CONFIGURATION BY DISABLING NETWORKING BY DEFAULT, FUCK YOU.
-STEAM_PATH=""
-
-echo "(!) Checking your linux distro for available fixes..."
-if [ $OS == "ubuntu" ]; then
-     echo "(?) Your steam path is different. This is why you weren't able to see TF2."
-     echo "(!) Fixing wrong steam paths..."
-     STEAM_PATH="/home/$USER/.steam/"
-     #not sure if this automatically fixes the firejail problem
-     sudo echo "restricted-network no" > sudo /etc/firejail/default.profile
-fi
-
-#not tested
-#debian and ubuntu might be the same i dont see any difference between ubuntu and debian.
-if [ $OS == "debian" ]; then
-     echo "(!) Fixing wrong steam paths..."
-     echo "(?) Your steam path is different. This is why you weren't able to see TF2."
-     STEAM_PATH="/home/$USER/.steam/"
-     #not sure if this automatically fixes the firejail problem
-     sudo echo "restricted-network no" > sudo /etc/firejail/default.profile
-fi
-
-#not tested
-if [ $OS == "arch" ]; then
-     echo "(?) Your steam path did not change. You should see TF2 now."
-     STEAM_PATH="/home/$USER/.local/share/Steam/"
-fi
-
-mkdir db
-
 clear
 echo Cathook Sandbox: Initial Setup 1 / 2
 echo ------------------------------------
@@ -92,7 +89,6 @@ echo "(!) Cloning cathook..."
 bash <(wget -qO- https://raw.githubusercontent.com/nullworks/One-in-all-cathook-install/master/install-all)
 
 
-# to do: no hardcoded values below.
 clear
 echo Cathook Sandbox: Initial Setup 2 / 2
 echo -------------------------------------
@@ -102,7 +98,7 @@ echo "(?) All you need to do is now ./start.sh!"
 echo "(?) Create a list of steam accounts, and save them to accounts.txt in username:password format."
 echo
 echo "(?) If you are experiencing problems, it's because the instance is seperated from your system. "
-echo "firejail --dns=1.1.1.1 --net=$INTERFACE --netns=cathookns${i} --noprofile --private=./user_instances/b${i} --name=b${i} --env=PULSE_SERVER=unix:/tmp/pulse.sock --env=DISPLAY=:0.0 bash"
+echo "firejail --dns=1.1.1.1 --net=enp3s0 --netns=catbotns0 --noprofile --private=./user_instances/b0 --name=b0 --env=PULSE_SERVER=unix:/tmp/pulse.sock --env=DISPLAY=:0.0 bash"
 echo "-- OR (if you have a bot sandbox ready) --"
 echo "firejail --join=b0 bash"
 echo
